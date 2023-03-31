047079cc672faa0a8cfdfdc5490c0c7a470bb7f3
Yousef Mahmoud <usef.mahmoud@outlook.com>
2023-03-31 19:21:38 +0200

    sending verification mail

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import register from './DB/registerUser'
 
 import './App.scss';
 function App() {
   
+  const signup = async () => {
+    const res = await register('yousef', 'mahmoud', 'mostafaahmad@proton.me', 'pass')
+
+    if(res.error == true){
+      console.log(res.message)
+    }else{
+      console.log('succeed')
+    }
+  }
+
   return (
     <div className="App">
-
+      <button onClick={signup}>
+        click me
+      </button>
     </div>
   )
 }
--- src/DB/registerUser.js
+++ src/DB/registerUser.js
@@ -1,13 +1,16 @@
 import { addDoc, collection, getDocs, query, queryEqual, where } from 'firebase/firestore'
 import { db } from './config'
+import emailjs from '@emailjs/browser'
+
 
 const register = async (first_name, last_name, email, password) => {
     const usersRef = await collection(db, "users")
+    const verificationCodesRef = await collection(db, 'verification_codes')
 
     const q = await query(usersRef, where('email', '==', email))
     const querySnapshot = await getDocs(q)
     if (querySnapshot.empty) {
-        await addDoc(usersRef, {
+        const currentUser = await addDoc(usersRef, {
             first_name: first_name,
             last_name: last_name,
             email: email,
@@ -18,13 +21,24 @@ const register = async (first_name, last_name, email, password) => {
             created_date: new Date(),
             public_profile: false,
             location: [0, 0],
-            activated: false
+            email_verified: false,
+            username: ''
+        })
+        await addDoc(verificationCodesRef, {
+            user_id: currentUser.id
+        }).then(verification => {
+            emailjs.send('service_amkssjq', 'template_py0217r', {
+                message: `https://coureroo.web.app/auth/verify-email/${verification.id}`,
+                user: email
+            }, 'y5DJWWU-uEfeGlLNV')
         })
 
         return await {
             error: false,
             message: '',
-            data: {}
+            data: {
+                user_id: currentUser.id
+            }
         }
     } else {
         return await {
