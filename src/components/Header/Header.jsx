85cb5633048d2a0fa0eef45baf83a897631b7798
Yousef Mahmoud <usef.mahmoud@outlook.com>
2023-04-01 09:02:58 +0200

    header with register buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,48 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import Logo from '../../assets/imgs/logo.png'
+import { GrSearch } from 'react-icons/gr'
+import { Link } from 'react-router-dom'
 
 import './Header.scss'
-const Header = () => {
+const Header = ({ is_loggedin }) => {
 
   return (
     <div className='main__header'>
-      
+      <div className="header__container">
+        <div className="main__section">
+          <Link to={'/'} className="header__logo">
+            <img src={Logo} alt="" />
+          </Link>
+          <div className="header__search">
+            <div className="search__box">
+              <input type="text" placeholder='search for courses' />
+              <span className="search__icon">
+                <GrSearch />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="user__section">
+          {
+            is_loggedin ?
+            <div>
+              LoggedIn
+            </div>
+            :
+            <div className='registering__buttons'>
+              <Link to={'/login'} className="login__btn">
+                Login
+              </Link>
+              <Link to={'/signup'} className="signup__btn">
+                Signup
+              </Link>
+            </div>
+          }
+        </div>
+      </div>
     </div>
   )
+
 }
 
 export default Header
--- src/pages/Signup/Singup.jsx
+++ src/pages/Signup/Singup.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../../components/Header/Header'
+import { useNavigate } from 'react-router-dom'
+
 
 import './Signup.scss'
+import { checkUserLoggedIn } from '../../funcs/checkUserLoggedIn'
 const Singup = () => {
+  const navigate = useNavigate()
+  const [isLoggedIn, setIsLoggedIn] = useState()
+
+  useEffect(() => {
+    const check = async () => {
+      let x = await checkUserLoggedIn()
+      await setIsLoggedIn(x)
+      if(await x){
+        navigate('/')
+      }
+    }
+
+    check()
+  }, [])
+
   return (
+
     <div className='signup-page'>
-        <Header />
+      <Header is_loggedin={isLoggedIn} />
     </div>
+
   )
 }
 
